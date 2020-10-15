a6d838af047580751cdedf8ec5c329bcf30eab37
Sammy <sammy.cullinan@gmail.com>
2020-10-15 13:21:08 -0700

    react app created

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const { authMiddleware } = require('./utils/auth');
+const express = require("express");
+const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
 //import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 
 //import typedefs and resolvers
-const { typeDefs, resolvers } = require('./schemas/index');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas/index");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,17 +15,24 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
 //integrate apollo server with express appilcation as middleware
 server.applyMiddleware({ app });
 
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
-db.once('open', () => {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
